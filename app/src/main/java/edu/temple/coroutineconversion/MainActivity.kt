6bf3028773b96cafc09f2dfa9b3761f8119bacec
Joe Michel <tun69277@temple.edu>
2023-11-02 14:52:39 -0400

    Final changes

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
@@ -31,22 +32,16 @@ class MainActivity : AppCompatActivity() {
         val scope= CoroutineScope(Job() + Dispatchers.Default)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
-            }.start()
+            scope.launch { imageDisplay() }
         }
     }
 
-    suspend fun messageDisplay(){
+    suspend fun imageDisplay(){
         repeat(100 ) {
             withContext(Dispatchers.Main){
                 handler.sendEmptyMessage(it)
             }
         }
-
         delay(40)
     }
 }
